17d3245db972c77980bf0948788cc9f14340eba7
zivuch <zivuch@gmail.com>
2022-08-16 18:46:36 +0300

    kkk

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors())
 // to extrat the data form the body
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
