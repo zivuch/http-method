7a1ffdc736fc8b8ade79d816b7e3b991f57baeea
zivuch <zivuch@gmail.com>
2022-12-04 16:25:28 +0200

    form method

--- server.js
+++ server.js
@@ -11,12 +11,35 @@ app.listen(process.env.PORT||5000,()=>{
   console.log('server is running on port 5000');
 });
 
-app.get('/login',(req,res)=>{
+app.get('/example',(req,res)=>{
   // console.log(req.query);
-  res.json({method:'GET',query:req.query})
+  res.send(`<div>
+    <h2>URL:${req.url}</h2>
+    <h2>method:${req.method}</h2>
+    <h2>query:${JSON.stringify(req.query)}</h2>
+    <h2>params:${JSON.stringify(req.params)}</h2>
+    <h2>body:${JSON.stringify(req.body)}</h2>
+    </div>`)
 })
 
-app.post('/login', (req,res)=>{
+app.get('/example/:param',(req,res)=>{
+  // console.log(req.query);
+  res.send(`<div>
+    <h2>URL:${req.url}</h2>
+    <h2>method:${req.method}</h2>
+    <h2>query:${JSON.stringify(req.query)}</h2>
+    <h2>params:${JSON.stringify(req.params)}</h2>
+    <h2>body:${JSON.stringify(req.body)}</h2>
+    </div>`)
+})
+
+app.post('/example', (req,res)=>{
   // console.log(req.body);
-  res.json({method:'POST',body:req.body})
+  res.send(`<div>
+    <h2>URL:${req.url}</h2>
+    <h2>method:${req.method}</h2>
+    <h2>query:${JSON.stringify(req.query)}</h2>
+    <h2>params:${JSON.stringify(req.params)}</h2>
+    <h2>body:${JSON.stringify(req.body)}</h2>
+    </div>`)
 })
